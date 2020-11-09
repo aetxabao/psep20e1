f1a4e5a5671eb41828d77aa11d7cb243647ae473
aechavao <aechavao@educacion.navarra.es>
2020-11-09 22:21:25 +0100

    code

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace taskprodcons
+{
+    class Caja{
+        private readonly object token = new object();
+        public int Valor {get;set;}
+        public Caja(int valor){
+            this.Valor = valor;
+        }
+        public void Incrementar(int valor){
+            //lock (token)
+            {
+                this.Valor += valor;
+            }
+        }
+        public void Decrementar(int valor){
+            //lock (token)
+            {
+                this.Valor -= valor;
+            }
+        }
+    }
+
+    class Program
+    {       
+        static void Main()
+        {
+            Caja caja = new Caja(0); 
+            int[] incrementos = {200,100,50,200,300,150,50,100,150,200};//1500
+            int[] decrementos = {50,150,200,300,150,50,100,200,100,200};//1500
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
+            Console.WriteLine("Caja: {0}",caja.Valor);
+        }
+    }
+}
