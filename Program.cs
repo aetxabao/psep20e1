cebf1eff467e1c736a791649c186a5f40b5842bd
aechavao <aechavao@educacion.navarra.es>
2020-11-09 22:23:37 +0100

    sol1

--- Program.cs
+++ Program.cs
@@ -31,18 +31,18 @@ namespace taskprodcons
             int[] incrementos = {200,100,50,200,300,150,50,100,150,200};//1500
             int[] decrementos = {50,150,200,300,150,50,100,200,100,200};//1500
 
+            Task t1 = Task.Run(() => {
+                for(int i=0;i<1000;i++)
+                    caja.Incrementar(incrementos[i%10]);
+            });
+            
+            Task t2 = Task.Run(() => {
+                for(int i=0;i<1000;i++)
+                    caja.Decrementar(decrementos[i%10]);
+            });
 
-
-
-
-
-
-
-
-
-
-
-
+            t1.Wait();
+            t2.Wait();
             
             Console.WriteLine("Caja: {0}",caja.Valor);
         }
